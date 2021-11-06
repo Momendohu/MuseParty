f64a5dda5eb9a9af621fa06791afba415959dd2e
escargot23@gmail.com <ueuetenango@ueuetenangonoMacBook-Pro.local>
2021-11-06 21:33:53 +0900

    update:一部usingの削除

--- Assets/Scripts/Core/EntityBase.cs
+++ Assets/Scripts/Core/EntityBase.cs
@@ -1,7 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class EntityBase {
+﻿public abstract class EntityBase {
     public abstract string getId ();
 }
