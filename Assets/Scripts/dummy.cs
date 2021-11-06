63161ef680fa141c8678d1faa8570174c89da0f0
escargot23@gmail.com <ueuetenango@ueuetenangonoMacBook-Pro.local>
2021-11-06 21:07:13 +0900

    add:ダミーファイル

---
+++ Assets/Scripts/dummy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dummy : MonoBehaviour{
+    void Awake(){
+        
+    }
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
